963d8e05ab995f1bae11087c1ee792c2435416ce
Oussama :) <s1114762@student.windesheim.nl>
2020-02-21 11:41:01 +0100

    fixed mix up with y and Z

--- scene.js
+++ scene.js
@@ -1,33 +1,24 @@
-var scene, camera, renderer;
+let scene, camera, renderer;
 
 init();
 
 // createCube({width: 5, height: 5, depth: 5}, {x: 0, y: 1, z: 0});
 // createPyramid({x: 10, y:10, z: 10});
 
-createHouse({height: 100, width: 100, depth: 100},  {x: -50, y: -200, z: -100});
-createHouse({height: 50, width: 50, depth: 50},  {x: 100, y: -200, z: -100});
-
+createHouse({height: 1000, width: 1000, depth: 1000},  {x: 0, y: 1000, z: -2000});
 
 function init() {
    // Create scene
-   scene = new THREE.Scene();
-
-   // Create camera
-   camera = new THREE.PerspectiveCamera(
-      75, // fov — Camera frustum vertical field of view.
-      window.innerWidth / window.innerHeight, // aspect — Camera frustum aspect ratio.
-      0.1, // near — Camera frustum near plane.
-      5000); // far — Camera frustum far plane. 
-
-   // Create renderer
-   renderer = new THREE.WebGLRenderer({
-      antialias: true,
-      alpha: true
-   });
-
-   renderer.setSize(window.innerWidth, window.innerHeight);
-   document.body.appendChild(renderer.domElement);
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(55,window.innerWidth/window.innerHeight,45,30000);
+  camera.position.set(0,0,0);
+  renderer = new THREE.WebGLRenderer({antialias:true});
+  renderer.setSize(window.innerWidth,window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+  let controls = new THREE.OrbitControls(camera);
+  controls.addEventListener('change', renderer);
+  controls.minDistance = 50;
+  controls.maxDistance = 1500;
 
    var directions = ["posx.jpg", "negx.jpg", "posy.jpg", "negy.jpg", "posz.jpg", "negz.jpg"];
    var materialArray = [];
@@ -51,17 +42,12 @@ function init() {
    var light = new THREE.DirectionalLight(0xffffff, 1.5);
    light.position.set(0, 0, 1);
    scene.add(light);
-
-   // move camera from center
-   camera.position.x = 0; //move right from center of scene
-   camera.position.y = 50; //move up from center of scene
-   camera.position.z = 10; //move camera away from center of scene
 }
 
 function createCube(dimension, position = {x: 0, y: 0, z: 0}, material =  new THREE.MeshNormalMaterial()) {
    var geometry = new THREE.CubeGeometry(dimension.height, dimension.width, dimension.depth);
    var cube = new THREE.Mesh(geometry, material);
-   cube.position.set(position.x, position.z, position.y);
+   cube.position.set(position.x, position.y, position.z);
    scene.add(cube);
    return cube;
 }
@@ -69,7 +55,7 @@ function createCube(dimension, position = {x: 0, y: 0, z: 0}, material =  new TH
 function createPyramid(dimension = {height: 4, width: 5}, position = {x: 0, y: 0, z: 0}, material =  new THREE.MeshNormalMaterial()) {
    var geometry = new THREE.CylinderGeometry(0, dimension.height, dimension.width, 4, 0)
    var pyramid = new THREE.Mesh(geometry, material);
-   pyramid.position.set(position.x, position.z , position.y);
+   pyramid.position.set(position.x, position.y, position.z);
    pyramid.rotation.y = 5.5;
    scene.add(pyramid);
    return pyramid;
@@ -97,7 +83,7 @@ function createHouse(dimension = {height, width, depth}, position = {x: 0, y: 0,
    var cubeMaterials = new THREE.MeshFaceMaterial( cubeMaterialArray );
 
    cube = createCube({width: dimension.width, height: dimension.height, depth: dimension.depth}, {x: position.x, y: position.y, z: position.z}, cubeMaterials);
-   createPyramid({width: dimension.width - 2, height: dimension.height * 0.71},{x: position.x, y: position.y, z:  position.z + (dimension.height - 1)});
+   createPyramid({width: dimension.width - 2, height: dimension.height * 0.71},{x: position.x, y: position.y + (dimension.height - 1), z:  position.z});
 }
 
 var render = function () {
